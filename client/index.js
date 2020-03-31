5cefed72631f3e0b20d3aac211d5e9971c684f4d
youssefwork <youssef.samih97@gmail.com>
2020-03-31 23:42:50 +0100

    middle work on like lyric

--- client/index.js
+++ client/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+
+import SongList from "./components/SongList";
+import App from "./components/App";
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail'
+
+import './style/style.css';
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList}/>
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="song/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
--- server/models/song.js
+++ server/models/song.js
@@ -11,7 +11,7 @@ const SongSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'lyric'
   }]
-});
+}, { usePushEach: true, useFindAndModify: false });
 
 SongSchema.statics.addLyric = function(id, content) {
   const Lyric = mongoose.model('lyric');
